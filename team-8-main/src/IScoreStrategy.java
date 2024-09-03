76c6967d12b708f7863ac36feacb2d097b501dfa
Bingjiun Miu <98665601+joseph123223@users.noreply.github.com>
2024-09-03 09:17:07 -0700

    Delete team-8-main directory

--- team-8-main/src/IScoreStrategy.java
+++
@@ -1,9 +0,0 @@
-/**
- * Write a description of class IScoreStrategy here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public interface IScoreStrategy {
-   public int doOperation(int num);
-}
